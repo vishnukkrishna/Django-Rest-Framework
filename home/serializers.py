c46ad6cae152c5dc5e3d0d43f7fac3bb0a5d42d8
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-12-18 19:56:59 +0530

    special_characters validation is added

--- home/serializers.py
+++ home/serializers.py
@@ -7,6 +7,11 @@ class PeopleSerializer(serializers.ModelSerializer):
     fields = '__all__'
 
   def validate(self, data):
+
+    special_characters = "!@#$%^&*()_+-=,<>/"
+    if any(c in special_characters for c in data['name']):
+      raise serializers.ValidationError('Name cannot contain special characters')
+
     if data['age'] < 18:
       raise serializers.ValidationError('Age should be grater than 18')
     return data
